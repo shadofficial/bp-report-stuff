c1ed61ca3c566e2eb6914bbdc68fbbc6a731a381
shadofficial <53861418+shadofficial@users.noreply.github.com>
2019-08-13 11:10:41 +0530

    update to setup nav in buddypress
    i have added a nav for user to see content they have reported  i have commited to my level of knowledge in php as i have already told you on buddypress support i am more of a front end guy. you can assign me the front end task if you want sorry if it has some error please review.

--- bp_reports_tab.php
+++ bp_reports_tab.php
@@ -19,4 +19,43 @@
  * datastore. If not, see http://www.gnu.org/licenses/.
  * 
  */
+public function moderator_nav() {
+        if ( !is_user_logged_in() || bp_displayed_user_id() != get_current_user_id() ) {
+                            return;
+                        } 
+  else {
+                   bp_core_new_nav_item( array(
+				          'name' => 'Reported',
+				          'slug' => 'reportedbyme',
+				          'screen_function' => '',// Add function which will show reported content by user
+				          'position' => 70,
+				         'default_subnav_slug' => 'content'
+			) );
+  
+                        
+                        $displayed_user_id = bp_displayed_user_id();
+                        $user_domain = ( ! empty( $displayed_user_id ) ) ? bp_displayed_user_domain() : bp_loggedin_user_domain();
 
+                        $reported_link = trailingslashit( $user_domain . 'reported' );
+                        
+                        // Add subnav items     
+                        bp_core_new_subnav_item( array(
+                            'name' => 'content',
+                            'slug' => 'content',
+                            'parent_url' => $reported_link,
+                            'parent_slug' => 'content',
+                            'screen_function' => '', // function to show content reported by user
+                            'position' => 10,
+                        ) );
+                        
+                        // Add subnav items
+                         bp_core_new_subnav_item( array(
+                                'name' => 'In process',
+                                'slug' => 'in process',
+                                'parent_url' => $reported_link,
+                                'parent_slug' => 'content',
+                                'screen_function' => '', // function which will handle the content which are in process to be reviewed
+                                'position' => 20,
+                            ) );
+                        }
+		}
